468be33d7c589665f81134371b7790a2e8ff7f9a
sangeethaalagappan <sangeetha.alagappan@berkeley.edu>
2013-04-27 19:38:59 -0700

    Update Vertex.java

--- graph/Vertex.java
+++ graph/Vertex.java
@@ -1,20 +1,47 @@
 //Vertex.java
 
 package graph;
+import list.*; 
 
 class Vertex {
     protected Object vertexValue;
+    protected DList vertexAdjacencyList;
+    protected int degree;
+    protected ListNode node;
     
-    void vertex(){
+    Vertex(){
         vertexValue = null;
+        vertexAdjacencyList = new DList();
+        node = null;
     }
  
-    void vertex(Object v){
+    Vertex(Object v){
         vertexValue = v;
+        vertexAdjacencyList = new DList();
+        node = null;
     }
    
     Object getVertex(){
         return vertexValue;
     }
     
+    int getDegree(){
+        return vertexAdjacencyList.length();
+    }
+    
+    DList getAdjacencyList(){
+       return vertexAdjacencyList;
+    }
+    
+    void insertEdge(Edge e){
+        this.vertexAdjacencyList.insertBack(e);
+    }
+    
+    ListNode getNode(){
+        return node;
+    }
+    
+    void setNode(ListNode d){
+        node = d;
+    }
 }   
