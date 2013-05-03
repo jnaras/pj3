75c1cce06a9201c8ddf95d6f4c67dc63a3b696ae
sangeethaalagappan <sangeetha.alagappan@berkeley.edu>
2013-05-02 22:42:01 -0700

    Added a few comments.

--- graph/Edge.java
+++ graph/Edge.java
@@ -1,7 +1,11 @@
-
 package graph;
 import list.*;
 
+/*
+* The Edge class represents our internal representation of an Edge object.
+* An edge object keeps track of its two vertices, its weight and its partner edge.
+* All its fields are protected.
+*/
 class Edge {
     protected Vertex v1;
     protected Vertex v2;
@@ -10,7 +14,15 @@ class Edge {
     protected ListNode node; //keeps track of which node it's inserted into in the adjacency list
      
     
-    //two constructors
+    /* Edge has two constructors*/
+
+   /* First constructor takes two vertices and a weight as input
+    * @param Vertex x is the first vertex.
+    * @param Vertex y is the second vertex.
+    * @param integer representing the weight of the edge.
+    * @return Edge constructed with appropriate end points and weight.
+    */
+    
     Edge(Vertex x, Vertex y, int w){
         weight = w;
         v1 = x;
@@ -24,6 +36,14 @@ class Edge {
         node = v1.getAdjacencyList().back();
     }
     
+    /* 
+     * Four parameter constructor for Edge class that creates an Edge after
+     * taking in an input of two vertices, a weight and the edge's partner.
+     * @param vertex x is the first vertex of the edge
+     * @param vertex y is the second vertex of the edge
+     * @param integer representing the weight of the edge
+     * @param Edge representing the Edge's partner
+     */
     Edge(Vertex x, Vertex y, int w, Edge partner){
         weight = w;
         v1 = x;
@@ -33,18 +53,34 @@ class Edge {
         node = v1.getAdjacencyList().back();
     }
     
+    /*
+     * get V1() returns the first vertex of an edge.
+     * @return Vertex representing the first vertex of the given edge.
+     */
     Vertex getV1(){
         return v1;
     }
     
+     /*
+     * get V2() returns the second vertex of an edge.
+     * @return Vertex representing the second vertex of the given edge.
+     */
     Vertex getV2(){
         return v2;
     }
     
+     /*
+     * getWeight() returns the weight of an edge.
+     * @return integer representing the weight of the given edge.
+     */
     int getWeight(){
         return weight;
     }
     
+     /*
+     * getPartner() returns the edge's partner.
+     * @return Edge representing the partner of the given edge.
+     */
     Edge getPartner(){
         return partner;
     }
